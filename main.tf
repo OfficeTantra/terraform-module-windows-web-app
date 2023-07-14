35a5bbe79d10be3a2bb8e7a7cbcffd76eac5af0d
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2023-07-14 11:43:11 +0100

    fixed issue with scm ip restrictions

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "azurerm_windows_web_app" "windows_web_app" {
 
     # Cloudflare or Azure Application Gateway
     dynamic "scm_ip_restriction" {
-      for_each = each.value.site_config.ip_restriction
+      for_each = each.value.site_config.scm_ip_restriction
 
       content {
         ip_address = scm_ip_restriction.value
@@ -371,7 +371,7 @@ resource "azurerm_windows_web_app_slot" "windows_web_app" {
 
     # Cloudflare or Azure Application Gateway
     dynamic "scm_ip_restriction" {
-      for_each = each.value.site_config.ip_restriction
+      for_each = each.value.site_config.scm_ip_restriction
 
       content {
         ip_address = scm_ip_restriction.value
@@ -516,5 +516,31 @@ resource "azurerm_windows_web_app_slot" "windows_web_app" {
   tags                      = each.value.tags
   virtual_network_subnet_id = each.value.virtual_network_subnet_integration_subnet_id
   zip_deploy_file           = each.value.zip_deploy_file
+
+}
+
+resource "azurerm_private_endpoint" "windows_web_app" {
+  for_each = {
+    for key, value in var.windows_web_apps : key => value
+    if value.enable_private_endpoint == true && value.cloudflare_protected == false
+  }
+
+  name                          = format("pep-%s", each.value.name)
+  location                      = each.value.location
+  resource_group_name           = each.value.resource_group_name
+  subnet_id                     = each.value.virtual_network_subnet_private_endpoint_id
+  custom_network_interface_name = format("nic-%s", each.value.name)
+
+  private_service_connection {
+    name                           = format("pl-%s", each.value.name)
+    private_connection_resource_id = azurerm_windows_web_app.windows_web_app[each.key].id
+    subresource_names              = ["sites"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "privatelink.azurewebsites.net"
+    private_dns_zone_ids = each.value.private_dns_zone_ids
+  }
 
 }
